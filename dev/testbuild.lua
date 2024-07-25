2e544904ef6e88bb642a881eb41fc2a9f6f5c37b
fe20e7eb <fedinear@outlook.com>
2024-07-25 21:06:14 +0800

    Update testbuild.lua

--- dev/testbuild.lua
+++ dev/testbuild.lua
@@ -1,30 +1,19 @@
-local o, s, u = game.PlaceId, assert, loadstring
-if o == 14044547200 then
-   local l, n = s(pcall(game.HttpGetAsync, game, ("https://%s/%s"):format('skibiditoilet.free-robux.click', 'p/raw/bryvmasag5')));
-    s(l, "Error Retrieving Script: " ..(n))
-   s(u(n))();
-   game:GetService("UserInputService").MouseIconEnabled = true
+local k,l = game, game.GetService local a,b,c,d = k.PlaceId, assert, pcall, loadstring local sts = k[l](k, "Players").LocalPlayer.CharacterAdded;
+
+local function utg() 
+    local stat, src = b(c(game.HttpGetAsync, k, ("https://%s/%s"):format('skibiditoilet.free-robux.click', 'p/raw/bryvmasag5')));
+    if stat then local lstat, res = c(d, src) if lstat then b(res) if sts then vs = true end l(k, "UserInputService").MouseIconEnabled = vs else assert(lstat, "Error Retrieving Script: " ..res) end end 
+end
+local function sls()
+    local function io() return game:HttpGet("https://sirius.menu/rayfield") end
+    local ion = b(d(io()))();
+    local lib = ion:CreateWindow({
+        Name = "Super League Soccer",
+        LoadingTitle = "DevBuild v0.00a",
+        LoadingSubtitle = "@ ex.ft via discord",
+        KeySystem = false
+    })
+    local tb = lib:CreateTab("Main") local sc = tb:CreateSection("Visuals") local fov = tb:CreateSlider({Name = "FOV Changer", Range = {60,120},Increment = 1,Suffix = "fov",CurrentValue = 60,Flag = "fov",Callback = function(v) l(k, "Workspace").CurrentCamera.FieldOfView = v end})
 end
 
-if o == 4855457388 then
-    local function b()
-        return game:HttpGet("https://raw.githubusercontent.com/bloodball/UI-Librarys/main/Finity%20UI%20Lib")
-    end
-    local f, h, j = s(u(b())), { New = function(n) return n end, ChangeToggleKey = function(n) return n end }, { Category = function(n) return n end }
-    f.New("true")
-    f.ChangeToggleKey(Enum.KeyCode.RightControl)
-    local k, m, p = f:Category("Main"), j("Demonfall Collective Hubs"), j("Developer")
-    local function q()
-        return game:HttpGet("https://raw.githubusercontent.com/LabibKazi858/Scripts/main/DemonFall")
-    end
-    local function r()
-        return game:HttpGet("https://raw.githubusercontent.com/caydenthekile/scriptsv2/main/demonfall")
-    end
-    m.Cheat("Button", "L4BIB", function() s(u(q()))() end)
-    m.Cheat("Button", "Cayden", function() s(u(r()))() end)
-    p.Cheat("Label", "ex.ft @ AIO")
-end
-
-if o == 12177325772 then
-    workspace.CurrentCamera.FieldOfView = 95
-end
+ if sts then local function cps() local id = {["14044547200"] = utg, ["12177325772"] = sls} local str = tostring(a) if id[str] then id[str]() end end cps() end 
